2fb019a16c1b7c1d6d1ae57beda538c359217880
hongyx <1104350235@qq.com>
2019-10-08 15:36:11 +0800

    react-router-dom && 首页组件的拆分

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { BrowserRouter, Route} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/index'
 import Header from './common/header/index'
 import { GlobaStyle } from './style'
+import Home from './pages/home'
+import Detail from './pages/detail'
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <GlobaStyle />
       <Provider store={store}>
         <Header />
+        <BrowserRouter>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/detail' exact component={Detail}></Route>
+        </BrowserRouter>
       </Provider>
     </div>
   );
